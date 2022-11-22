fd06fdf9c1bfc14dab35ebe81d8eaff56dc06bea
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-11-22 13:31:18 +0100

    initial project setup with firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOPZjwknIFTXU1sm4-Mo1ByZYZE9QUf9k",
+  authDomain: "chubby-dog-d57a4.firebaseapp.com",
+  projectId: "chubby-dog-d57a4",
+  storageBucket: "chubby-dog-d57a4.appspot.com",
+  messagingSenderId: "373666821649",
+  appId: "1:373666821649:web:247866ed5447c6b38723b0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
