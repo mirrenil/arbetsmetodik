cdc4ea97cca5a6d28687571d8ae3909d6a0c0271
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-11-23 14:18:53 +0100

    preparation for db connection

--- src/Pages/NewListingPage.tsx
+++ src/Pages/NewListingPage.tsx
@@ -1,127 +1,215 @@
-import React, { useRef, useState } from 'react';
-import Box from '@mui/material/Box';
-import { Button, TextField} from '@mui/material';
+import React, { FormEvent, useRef, useState } from "react";
+import Box from "@mui/material/Box";
+import { Button, FormGroup, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { storage } from "../firebase";
+import { getStorage, getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { db } from "../firebase";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { display } from "@mui/system";
+//import { useAuth } from '../contexts/AuthContext';
 
 const categories = [
   {
-    value: '',
-    label: '',
+    value: "",
+    label: "",
   },
   {
-    value: 'Electronics',
-    label: 'Electronics',
+    value: "Electronics",
+    label: "Electronics",
   },
   {
-    value: 'Home',
-    label: 'Home',
+    value: "Home",
+    label: "Home",
   },
   {
-    value: 'Clothing',
-    label: 'Clothing',
+    value: "Clothing",
+    label: "Clothing",
   },
   {
-    value: 'Other',
-    label: 'Other',
+    value: "Other",
+    label: "Other",
   },
 ];
 
 const locations = [
   {
-    value: '',
-    label: '',
+    value: "",
+    label: "",
   },
   {
-    value: 'Stockholm',
-    label: 'Stockholm',
+    value: "Stockholm",
+    label: "Stockholm",
   },
   {
-    value: 'Gothenburg',
-    label: 'Gothenburg',
+    value: "Gothenburg",
+    label: "Gothenburg",
   },
   {
-    value: 'Malmö',
-    label: 'Malmö',
+    value: "Malmö",
+    label: "Malmö",
   },
 ];
 
 function NewListing() {
-  const [category, setCategory] = useState('Select Category');
-  const [location, setLocation] = useState('Select Location');
-  const uploadImageRef = useRef<HTMLInputElement | null>(null);
-  
-  const onUpload = () => {
-    uploadImageRef.current?.click();
-  }
+  /* const { currentUser } = useAuth() */
+  const [category, setCategory] = useState("Select Category");
+  const [location, setLocation] = useState("Select Location");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState([]);
+  const listingsRef = collection(db, "listings");
+  const navigate = useNavigate();
+
+  // type any for now
+  const handleNewListing = async (event: FormEvent) => {
+    event.preventDefault();
+    console.log("hello test");
+      await addDoc(listingsRef, {
+        category,
+        title,
+        description,
+        price,
+        location,
+        createdAt: Timestamp.now().toDate(),
+      });
+    console.log("New listing added");
+    navigate("/");
+  };
+
+  const handleImage = () => {
+    // TODO: add image upload
+    // const multipleImages = [];
+    // for (let i = 0; i < e.target.files.length; i++) {
+    //   multipleImages.push(e.target.files[i]);
+    // }
+    // multipleImages.push(e.target.files);
+    // setImage(multipleImages);
+  };
+
+  const uploadpload = async (image: string[]) => {
+    console.log(image);
+ // prepare for upload
+    // await image.forEach((image) => {
+    //   const imageRef = ref(storage, `images/${image.name}`);
+    //   uploadBytes(imageRef, image).then(() => {
+    //     console.log('Upload successful');
+    //   });
+  };
 
   return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-      <h1>Create a listing</h1>
-<Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' }, display: "flex", flexDirection: "column", alignItems: "center"
-      }}
-      noValidate
-      autoComplete="off"
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center", height: "100vh", margin: "auto" }}
     >
-      <TextField
-          id="outlined-select-category-native"
-          select
-          label="Select a category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          SelectProps={{
-            native: true,
-          }}
-        >
-          {categories.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </TextField>
-      <TextField id="outlined-basic" label="Title" variant="outlined" />
-      <TextField
-          id="outlined-multiline-static"
-          multiline
-          rows={4}
-          label="Description"
-        />
-        <TextField
-          id="outlined-select-category-native"
-          select
-          label="Select your location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          SelectProps={{
-            native: true,
-          }}
-        >
-          {locations.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </TextField>
-        <TextField id="outlined-basic" label="Price per day" variant="outlined" />
-        
-        {/* FANCIER DATE PICKER HERE IF WE HAVE TIME */}
-        
-         <input type="datetime-local" name="date" id="date" />
-         <Box sx={{border: "1px dashed grey", height: "4rem", borderRadius: "6px"}}>
-            <input style={{padding: "1rem"}}
-        ref={uploadImageRef}
-        type="file"
-        accept='image/*'
-        onChange={onUpload} />
-         </Box>
-        
-        <Box sx={{backgroundColor: "#80CCFF", width: "70vw", borderRadius: "6px", opacity: "60%"}}>
-          <h5 style={{ margin: "1rem"}}>By adding this listing you agree with Chubby Dog's terms of use</h5>
-      
-        </Box>
-        <Button variant="contained" size='large'>Create listing</Button>
-    </Box>
+      <h1>Create a listing</h1>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+          display: "flex",
+          flexDirection: "column",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <FormGroup onSubmit={handleNewListing}>
+          <TextField
+          sx={{marginBottom: "1rem"}}
+            id="outlined-select-category-native"
+            select
+            label="Select a category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            SelectProps={{
+              native: true,
+            }}
+          >
+            {categories.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </TextField>
+          <TextField
+          sx={{marginBottom: "1rem"}}
+            id="outlined-basic"
+            label="Title"
+            variant="outlined"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextField
+          sx={{marginBottom: "1rem"}}
+            id="outlined-multiline-static"
+            multiline
+            rows={4}
+            label="Description"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <TextField
+          sx={{marginBottom: "1rem"}}
+            id="outlined-select-category-native"
+            select
+            label="Select your location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            SelectProps={{
+              native: true,
+            }}
+          >
+            {locations.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </TextField>
+          <TextField
+          sx={{marginBottom: "1rem"}}
+            id="outlined-basic"
+            label="Price per day"
+            variant="outlined"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+
+          {/* FANCIER DATE PICKER HERE IF WE HAVE TIME */}
+     
+          <input type="date" name="date" id="date" />
+         
+          <div
+            style={{
+              border: "1px dashed grey",
+              borderRadius: "6px",
+              margin: "1rem",
+            }}
+          >
+            <input
+              style={{ padding: "1rem" }}
+              type="file"
+              onChange={() => handleImage()}
+              name="image"
+              id="image"
+              accept="image/png, image/jpeg"
+              multiple
+            />
+          </div>
+
+          <Box
+            sx={{
+              backgroundColor: "#80CCFF",
+              borderRadius: "6px",
+              opacity: "60%",
+              marginBottom: "1rem",
+            }}
+          >
+            <h5 style={{ margin: "1rem" }}>
+              By adding this listing you agree with Chubby Dog's terms of use
+            </h5>
+          </Box>
+          <Button type="submit" variant="contained" size="large">
+            Create listing
+          </Button>
+        </FormGroup>
+      </Box>
     </div>
   );
 }
