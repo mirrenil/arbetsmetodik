5fe17cf2776462fafbb5f30f2a5170122de5527e
linnea <linnea.albertsson@gmail.com>
2022-11-29 11:50:54 +0100

    Added wrapper for requests card

--- src/Components/ReceivedReqCard.tsx
+++ src/Components/ReceivedReqCard.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Box, CardMedia, SxProps, Typography, useTheme } from '@mui/material';
+import React from 'react';
+import camera from '../Assets/Images/Film-Photography.png'
 
 const ReceivedReqCard = () => {
-  return (
-    <div>ReceivedReqCard</div>
-  )
+	const theme = useTheme();
+
+	return (
+		<Box
+			sx={{
+				boxShadow: '0px 0px 15px -3px #000000',
+				width: '25rem',
+				height: '15rem',
+				borderRadius: theme.shape.buttonBorderRadius,
+        margin: 'auto'
+			}}
+		>
+      <CardMedia sx={imgStyle} component="img" src={camera} />
+      <Typography>Request from:</Typography>
+      <Typography>Request for:</Typography>
+		</Box>
+	);
+};
+
+const imgStyle: SxProps = {
+  width: '7rem',
+  objectFit: 'scale-down'
 }
 
-export default ReceivedReqCard
+export default ReceivedReqCard;
