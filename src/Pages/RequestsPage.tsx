0582e47eba81a9bddd8a3d4bfc4c5069d7e0a27d
linnea <linnea.albertsson@gmail.com>
2022-11-29 16:56:22 +0100

    Added styling for decline btn

--- src/Pages/RequestsPage.tsx
+++ src/Pages/RequestsPage.tsx
@@ -1,15 +1,35 @@
 import { Typography } from '@mui/material';
-import React from 'react';
-import ReceivedReqCard from '../Components/ReceivedReqCard'
+import React, { useEffect, useState } from 'react';
+import ReceivedReqCard from '../Components/ReceivedReqCard';
+import { useAuth } from '../authContext';
+import { getDocs, collection, where, query } from 'firebase/firestore';
+import { db } from '../firebase';
+import { IRequest } from '../Interfaces';
 
 function RequestsPage() {
-  return (
-    <div>
-      <Typography>Recieved requests:</Typography>
-      {/* for each request current user has */}
-      <ReceivedReqCard />
-    </div>
-  );
+	const [myRequests, setMyRequests] = useState<IRequest[]>();
+	const { currentUser } = useAuth();
+	console.log(currentUser?.uid);
+
+	const getAllData = async () => {
+		const data = query(
+			collection(db, 'requests'),
+			where('toUser', '==', `${currentUser?.uid}`)
+		);
+		const req = await getDocs(data);
+		req.forEach((doc) => {
+			//set myRequest
+		});
+	};
+
+	getAllData();
+
+	return (
+		<div>
+			<Typography>Recieved requests:</Typography>
+			<ReceivedReqCard />
+		</div>
+	);
 }
 
 export default RequestsPage;
