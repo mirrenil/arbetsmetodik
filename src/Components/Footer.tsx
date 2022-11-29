0f18a5a6cd2c0a91c38201f46a0a4bc1cabdab5b
Erikisak <erik.isak@icloud.com>
2022-11-29 10:43:19 +0100

    #26 Updated footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,23 +5,22 @@ import footer2Img from '../Assets/Images/footer2.png'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { fontSize } from '@mui/system';
 
 const Footer = () => {
   return (
-    <Box sx={navBox}>
+    <Box >
         <Box sx={imgAlign}>
           <img src={footerImg} alt="" style={{width: '100%', height: '150px' }} />
           </Box>
-       <Box sx={align1}>
-      <Box sx={align}>
-      <Box sx={textalign}>
+       <Box sx={mobileContainer}>
+      <Box sx={mobileTextAlign}>
+      <Box >
         About Chubby Dog
       </Box>
-      <Box sx={textalign}>
+      <Box >
         How it works
       </Box>
-      <Box sx={textalign}>
+      <Box >
         Terms and conditions 
       </Box>
       </Box>
@@ -31,60 +30,44 @@ const Footer = () => {
         <FacebookIcon />
       </Box>
       </Box> 
-
-      <Box sx={test}>
+      <Box sx={desktopContainer}>
       <Box sx={desktopAlign}>
           <img src={footer2Img} alt="" style={{width: '100%', height: 'auto' }} />
           </Box>
-      <Box sx={align2}>
-      <Box sx={textAlign}>
+      <Box sx={desktopTextAlign}>
+      <Box sx={textStyle}>
         About Chubby Dog
       </Box>
-      <Box sx={textAlign}>
+      <Box sx={textStyle}>
         How it works
       </Box>
-      <Box sx={textAlign}>
+      <Box sx={textStyle}>
         Terms and conditions
       </Box>
-
       </Box>
-
       <Box sx={iconAlignDesktop}>
         <TwitterIcon sx={iconStyle} />
         <InstagramIcon sx={iconStyle}/>
         <FacebookIcon sx={iconStyle}/>
       </Box>
-
-
       </Box>
-
-
-
-
-
-      </Box>
-
-      
-      
+      </Box> 
   )
 }
 
-const navBox: SxProps = {
-}
-const test: SxProps = {
+const desktopContainer: SxProps = {
   position: 'relative',
   display: { xs: 'none', md:'flex', lg: 'flex', xl: 'flex' },
-  
 }
-const textAlign: SxProps = {
+const textStyle: SxProps = {
   marginBottom: '30px'
 }
-const align1: SxProps = {
+const mobileContainer: SxProps = {
   display: { xs: 'flex', md:'none', lg: 'none', xl: 'none' },
   flexDirection: 'column',
   paddingBottom: '20px',
 }
-const align2: SxProps = {
+const desktopTextAlign: SxProps = {
   display: { xs: 'none', md:'flex', lg: 'flex', xl: 'flex' },
   flexDirection: 'column',
   position: 'absolute',
@@ -93,23 +76,18 @@ const align2: SxProps = {
   fontSize: { xs: 'none', md:'20px', lg: '30px', xl: '30px' },
   boxSizing: 'border-box'
 }
-const align: SxProps = {
+const mobileTextAlign: SxProps = {
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'center',
   gap: '10px',
-  marginLeft: '50px',
-  
-
-  
+  marginLeft: '50px',  
 }
 const imgAlign: SxProps = {
   display: { xs: 'flex', md:'none', lg: 'none', xl: 'none' },
-  
 }
 const desktopAlign: SxProps = {
   display: { xs: 'none', md:'flex', lg: 'flex', xl: 'flex' },
-  
 }
 const iconAlign: SxProps = {
   display: 'flex',
@@ -127,8 +105,5 @@ const iconAlignDesktop: SxProps = {
 }
 const iconStyle: SxProps = {
   fontSize: { xs: 'none', md:'60px', lg: '70px', xl: '70px' },
-}
-const textalign: SxProps = {
-  
 }
 export default Footer
