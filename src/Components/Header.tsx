6e286a4469f9dc98b878bb6cde81548d0c846757
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-12-02 11:53:11 +0100

    if no userImg exist we show the same as on the profile

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -32,145 +32,164 @@ const Header = () => {
   }, []);
   const navigate = useNavigate();
 
-	const handleSubmit = () => {
-		logout()
+  const handleSubmit = () => {
+    logout();
     navigate("/");
-    
   };
-  
+
   return (
     <Box sx={navBox}>
       <Box sx={navBoxInnerMobile}>
-      <Box sx={logo}>
-        <Link to='/'>
-        <Avatar alt="Logo" src={logoImg} sx={logoImgStyle}/>
-        </Link>
-      </Box>
-      <Box>
-        <Box sx={navItems}>
-        <Link to='/newlisting' style={{textDecoration: 'none'}}>
-          <AddCircleOutlineIcon sx={{cursor: 'pointer', width: '30px', height: '30px', fill: '#000'}} />
-        </Link>
-        {currentUser ? 
-				<Box>
-          <Link to='/profile/:id' style={{textDecoration: 'none'}}>
-						{userImg ? 
-							<Box
-							component="img"
-							src={userImg}
-							alt={userName}
-							sx={{
-								cursor: 'pointer', 
-								width: '30px', 
-								height: '30px', 
-								borderRadius: '50px',
-							}}
-						/> : 
-						<Box
-							sx={{
-								cursor: 'pointer', 
-								width: '30px', 
-								height: '30px', 
-								borderRadius: '50px',
-							}}
-						>
-							<Typography sx={{color: '#000'}}>{userName?.charAt(0)}</Typography>
-						</Box>
-					}
+        <Box sx={logo}>
+          <Link to="/">
+            <Avatar alt="Logo" src={logoImg} sx={logoImgStyle} />
           </Link>
-        </Box> : 
-        <Link to='/profile/:id' style={{textDecoration: 'none'}}>
-          <AccountCircleIcon sx={{cursor: 'pointer', width: '30px', height: '30px', fill: '#000'}}  />          
-        </Link>
-        }
-          <MenuIcon
-            onClick={() => menuOpen ? setMenuOpen(false) : setMenuOpen(true)}
-            sx={{cursor: 'pointer', width: '30px', height: '30px', fill: '#000'}}
-          />
-          {menuOpen ? (
-            <Box 
-            sx={mobileMenuList}
-            data-aos="fade-left"
-            data-aos-offset="200"
-            data-aos-duration="1000"
-            >
-              <ul 
-              style={{listStyle: 'none', marginTop: '4em'}}
-              data-aos="fade-left"
-              data-aos-offset="200"
-              data-aos-duration="1000"
-              >
-                 
-                  <Link to='/signup' style={{textDecoration: 'none'}}>
-                    <Box 
-										sx={navItem}
-										onClick={ () => setMenuOpen(false)}
-										>
-                    <SensorOccupiedIcon sx={navMenuIcon}/><Typography sx={navItemText}>Sign Up</Typography>
-                    </Box>
-                  </Link>
-                  <Link to='/' style={{textDecoration: 'none'}}>
-									<Box 
-										sx={navItem}
-										onClick={ () => setMenuOpen(false)}
-										>
-                      <HelpOutlineIcon sx={navMenuIcon} /><Typography sx={navItemText}>How it works?</Typography>
-                    </Box>
-                  </Link>
-                  {currentUser ? 
-                   <Link to='/requests' style={{textDecoration: 'none'}}>
-                    <Box sx={navItem}>
-                      <AddCircleOutlineIcon sx={navMenuIcon}/><Typography sx={navItemText}>My requests</Typography>
-                   </Box>
-                  </Link>
-                  :
-                  null
-                  }
-                 
-                  <Link to='/' style={{textDecoration: 'none'}}>
-									<Box 
-										sx={navItem}
-										onClick={ () => setMenuOpen(false)}
-										>
-                    <GavelIcon sx={navMenuIcon} /><Typography sx={navItemText}>Terms of use</Typography>
-                    </Box>
-                  </Link>
-                    <Link to='/' style={{textDecoration: 'none'}}>
-										<Box 
-										sx={navItem}
-										onClick={ () => setMenuOpen(false)}
-										>
-                    <ContactPhoneIcon sx={navMenuIcon} /><Typography sx={navItemText}>Contact</Typography>
-                    </Box>
-                  </Link>
-										<Link to='/newlisting' style={{textDecoration: 'none'}}>
-										<Box 
-										sx={navItem}
-										onClick={ () => setMenuOpen(false)}
-										>
-												<AddCircleOutlineIcon sx={navMenuIcon}/><Typography sx={navItemText}>List an Item</Typography>
-											</Box>
-                    </Link>
-                    {currentUser ? 
-                    <>
-                        <Box sx={navItem}
-												onClick={ () =>{ 
-													setMenuOpen(false)
-													handleSubmit()
-													}}
-												>
-                          <LoginIcon sx={navMenuIcon}/><Typography sx={navItemText}>Logout</Typography>
-                        </Box>
-                    </>  :
-                      <Link to='/signin' style={{textDecoration: 'none'}}>
-                       <Box 
-												sx={navItem}
-												onClick={ () => setMenuOpen(false)}
-												>
-                          <LoginIcon sx={navMenuIcon}/><Typography sx={navItemText}>Login</Typography>
-                        </Box>
-                      </Link>
+        </Box>
+        <Box>
+          <Box sx={navItems}>
+            <Link to="/newlisting" style={{ textDecoration: "none" }}>
+              <AddCircleOutlineIcon
+                sx={{
+                  cursor: "pointer",
+                  width: "30px",
+                  height: "30px",
+                  fill: "#000",
+                }}
+              />
+            </Link>
+            {currentUser ? (
+              <Box>
+                <Link to="/profile/:id" style={{ textDecoration: "none" }}>
+                  {userImg ? (
+                    <Box
+                      component="img"
+                      src={
+                        currentUser.photoURL
+                          ? userImg
+                          : "https://thumbs.dreamstime.com/b/man-profile-cartoon-smiling-round-icon-vector-illustration-graphic-design-135443422.jpg"
                       }
+                      alt={userName}
+                      sx={{
+                        cursor: "pointer",
+                        width: "30px",
+                        height: "30px",
+                        borderRadius: "50px",
+                        color: "red",
+                      }}
+                    />
+                  ) : (
+                    <Box
+                      component="img"
+                      src={
+                        "https://thumbs.dreamstime.com/b/man-profile-cartoon-smiling-round-icon-vector-illustration-graphic-design-135443422.jpg"
+                      }
+                      alt={userName}
+                      sx={{
+                        cursor: "pointer",
+                        width: "30px",
+                        height: "30px",
+                        borderRadius: "50px",
+                      }}
+                    />
+                  )}
+                </Link>
+              </Box>
+            ) : (
+              <Link to="/profile/:id" style={{ textDecoration: "none" }}>
+                <AccountCircleIcon
+                  sx={{
+                    cursor: "pointer",
+                    width: "30px",
+                    height: "30px",
+                    fill: "#000",
+                  }}
+                />
+              </Link>
+            )}
+            <MenuIcon
+              onClick={() =>
+                menuOpen ? setMenuOpen(false) : setMenuOpen(true)
+              }
+              sx={{
+                cursor: "pointer",
+                width: "30px",
+                height: "30px",
+                fill: "#000",
+              }}
+            />
+            {menuOpen ? (
+              <Box
+                sx={mobileMenuList}
+                data-aos="fade-left"
+                data-aos-offset="200"
+                data-aos-duration="1000"
+              >
+                <ul
+                  style={{ listStyle: "none", marginTop: "4em" }}
+                  data-aos="fade-left"
+                  data-aos-offset="200"
+                  data-aos-duration="1000"
+                >
+                  <Link to="/signup" style={{ textDecoration: "none" }}>
+                    <Box sx={navItem} onClick={() => setMenuOpen(false)}>
+                      <SensorOccupiedIcon sx={navMenuIcon} />
+                      <Typography sx={navItemText}>Sign Up</Typography>
+                    </Box>
+                  </Link>
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    <Box sx={navItem} onClick={() => setMenuOpen(false)}>
+                      <HelpOutlineIcon sx={navMenuIcon} />
+                      <Typography sx={navItemText}>How it works?</Typography>
+                    </Box>
+                  </Link>
+                  {currentUser ? (
+                    <Link to="/requests" style={{ textDecoration: "none" }}>
+                      <Box sx={navItem}>
+                        <AddCircleOutlineIcon sx={navMenuIcon} />
+                        <Typography sx={navItemText}>My requests</Typography>
+                      </Box>
+                    </Link>
+                  ) : null}
+
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    <Box sx={navItem} onClick={() => setMenuOpen(false)}>
+                      <GavelIcon sx={navMenuIcon} />
+                      <Typography sx={navItemText}>Terms of use</Typography>
+                    </Box>
+                  </Link>
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    <Box sx={navItem} onClick={() => setMenuOpen(false)}>
+                      <ContactPhoneIcon sx={navMenuIcon} />
+                      <Typography sx={navItemText}>Contact</Typography>
+                    </Box>
+                  </Link>
+                  <Link to="/newlisting" style={{ textDecoration: "none" }}>
+                    <Box sx={navItem} onClick={() => setMenuOpen(false)}>
+                      <AddCircleOutlineIcon sx={navMenuIcon} />
+                      <Typography sx={navItemText}>List an Item</Typography>
+                    </Box>
+                  </Link>
+                  {currentUser ? (
+                    <>
+                      <Box
+                        sx={navItem}
+                        onClick={() => {
+                          setMenuOpen(false);
+                          handleSubmit();
+                        }}
+                      >
+                        <LoginIcon sx={navMenuIcon} />
+                        <Typography sx={navItemText}>Logout</Typography>
+                      </Box>
+                    </>
+                  ) : (
+                    <Link to="/signin" style={{ textDecoration: "none" }}>
+                      <Box sx={navItem} onClick={() => setMenuOpen(false)}>
+                        <LoginIcon sx={navMenuIcon} />
+                        <Typography sx={navItemText}>Login</Typography>
+                      </Box>
+                    </Link>
+                  )}
                 </ul>
               </Box>
             ) : null}
