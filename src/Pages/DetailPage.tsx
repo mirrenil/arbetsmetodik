4242f6c09870bd058f7eaaa61f0daa92cae97281
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-12-05 17:39:05 +0100

    crud works for title

--- src/Pages/DetailPage.tsx
+++ src/Pages/DetailPage.tsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../firebase";
-import { IListItem, IRequest } from "../Interfaces";
+import { IListItem } from "../Interfaces";
 import {
   Box,
   Card,
@@ -11,17 +18,30 @@ import {
   Typography,
   Button,
   SxProps,
+  DialogContent,
+  DialogContentText,
+  Modal,
+  TextField,
 } from "@mui/material";
 import { CSSProperties } from "@mui/styled-engine";
 import Dave from "../Assets/Images/Dave.png";
 import { useAuth } from "../Contexts/AuthContext";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { Clear, Edit } from "@mui/icons-material";
 
 function DetailPage() {
   const listingCollection = collection(db, "listings");
   const { id } = useParams();
   const [item, setItem] = useState<IListItem>();
-  const { currentUser } = useAuth()
+  const { currentUser } = useAuth();
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
+  const [title, setTitle] = useState<string>("");
+  // const [description, setDescription] = useState<string>("");
+  // const [price, setPrice] = useState<string>("");
+  // const [image, setImage] = useState<string>("");
+  // const [location, setLocation] = useState<string>("");
 
   useEffect(() => {
     async function setDocumentData() {
@@ -43,14 +63,32 @@ function DetailPage() {
   const handleSendRequest = async (e?: Event) => {
     const newRequest = {
       accepted: false,
-      createdAt: new Date,
+      createdAt: new Date(),
       fromUser: currentUser?.uid,
       itemId: item?.id,
       priceTotal: item?.price,
-      toUser: item?.authorID
-    }
+      toUser: item?.authorID,
+    };
     const docRef = await addDoc(collection(db, "requests"), newRequest);
-  }
+  };
+
+  const deleteListing = async (id: string) => {
+    const itemToRemove = doc(db, "listings", id);
+    await deleteDoc(itemToRemove);
+    alert("Listing with id " + id + " has been deleted");
+  };
+
+  const updateListing = async (id: string) => {
+    const itemToUpdate = doc(db, "listings", id);
+    await updateDoc(itemToUpdate, {
+      title: title,
+      // price: price,
+      // description: description,
+      // image: image,
+      // location: location,
+    });
+    alert("Listing with id " + id + " has been updated");
+  };
 
   return (
     <Box sx={wrapper}>
@@ -65,6 +103,103 @@ function DetailPage() {
         </Box>
         <CardContent sx={infoContainer}>
           <Box sx={cardHeaders}>
+            {currentUser?.uid === item?.authorID && (
+              <>
+                <Box sx={crudItems}>
+                  <button
+                    style={{
+                      border: "none",
+                      cursor: "pointer",
+                      backgroundColor: "transparent",
+                    }}
+                    onClick={() => deleteListing(item?.id)}
+                  >
+                    <Clear />
+                  </button>
+                  <button
+                    style={{
+                      border: "none",
+                      cursor: "pointer",
+                      backgroundColor: "transparent",
+                      padding: "1rem",
+                    }}
+                    onClick={handleOpen}
+                  >
+                    <Edit />
+                  </button>
+                </Box>
+                <Modal open={modalOpen} onClose={handleClose}>
+                  <Box sx={modalStyle}>
+                    <form onSubmit={() => updateListing(item?.id)}>
+                      <DialogContent sx={crudModal}>
+                        <DialogContentText>
+                          <Typography variant="h6" component="h6">
+                            Update your listing
+                          </Typography>
+                        </DialogContentText>
+                        <TextField
+                          autoFocus
+                          margin="normal"
+                          type="text"
+                          label="Title"
+                          variant="standard"
+                          value={title || ""}
+                          required
+                          onChange={(e) => setTitle(e.target.value)}
+                        />
+                        {/* <TextField
+                          autoFocus
+                          margin="normal"
+                          type="number"
+                          label="Price"
+                          variant="standard"
+                          value={price || ""}
+                          required
+                          onChange={(e) => setPrice(e.target.value)}
+                        /> */}
+                        {/* <TextField
+                          autoFocus
+                          margin="normal"
+                          type="text"
+                          label="Description"
+                          variant="standard"
+                          value={description || ""}
+                          required
+                          onChange={(e) => setDescription(e.target.value)}
+                        /> */}
+                        {/* <TextField
+                          autoFocus
+                          margin="normal"
+                          type="text"
+                          label="Location"
+                          variant="standard"
+                          value={location || ""}
+                          required
+                          onChange={(e) => setLocation(e.target.value)}
+                        /> */}
+                        {/* <TextField
+                          autoFocus
+                          margin="normal"
+                          type="text"
+                          label="Image URL"
+                          variant="standard"
+                          value={image || ""}
+                          required
+                          onChange={(e) => setImage(e.target.value)}
+                        /> */}
+                        <Button
+                          variant="contained"
+                          type="submit"
+                          sx={{ marginTop: "1.5rem" }}
+                        >
+                          Update Listing
+                        </Button>
+                      </DialogContent>
+                    </form>
+                  </Box>
+                </Modal>
+              </>
+            )}
             <Typography
               sx={titlePrice}
               gutterBottom
@@ -122,7 +257,11 @@ function DetailPage() {
           </Box>
           <CardMedia component="img" sx={lessorPic} image={Dave} alt="Dave" />
         </Card>
-        <Button sx={button} variant="contained" onClick={() => handleSendRequest()}>
+        <Button
+          sx={button}
+          variant="contained"
+          onClick={() => handleSendRequest()}
+        >
           Send a request
         </Button>
       </Card>
@@ -211,6 +350,30 @@ const button: SxProps = {
   marginBottom: "2rem",
   width: "40%",
   height: "15%",
+};
+
+const crudItems = {
+  display: "flex",
+  flexDirection: "row",
+};
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  height: 500,
+  bgcolor: "background.paper",
+  border: "none",
+  borderRadius: "6px",
+  boxShadow: 24,
+  p: 4,
+};
+
+const crudModal = {
+  display: "flex",
+  flexDirection: "column",
 };
 
 export default DetailPage;
