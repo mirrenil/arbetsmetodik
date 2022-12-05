e9cb6b9234276670bc6f0a68cf7058e50fdefe72
Hazem Kawas <hazem.k.4488@gmail.com>
2022-12-05 14:42:49 +0100

    add free rent category and page

--- src/Components/CategoriesContainer.tsx
+++ src/Components/CategoriesContainer.tsx
@@ -4,10 +4,11 @@ import AOS from "aos";
 import { useItems } from "../Contexts/ItemContext";
 import { useNavigate } from "react-router-dom";
 import "aos/dist/aos.css";
+import freePriceImage from "../Assets/Images/free.jpg";
 
 const CategoryCard = () => {
   const navigate = useNavigate();
-  AOS.init({once: true});
+  AOS.init({ once: true });
   const { categories, fetchCategoriesFromDb, setSelectedCategoryId } =
     useItems();
   const [showMore, setShowMore] = useState(false);
@@ -86,6 +87,29 @@ const CategoryCard = () => {
             />
           </Box>
         ))}
+        <Box
+          sx={categoryDiv}
+          onClick={() => {
+            navigate(`/category/free`);
+          }}
+          data-aos="fade-left"
+          data-aos-offset="200"
+          data-aos-duration="1000"
+        >
+          <Box sx={categoryTitleDiv}>
+            <Typography sx={categoryTitle}>Free</Typography>
+          </Box>
+          <Box
+            component="img"
+            sx={{
+              width: { xs: "150px", md: "150", lg: "200px", xl: "250px" },
+              height: { xs: "100px", md: "100px", lg: "150px", xl: "200px" },
+              borderRadius: "10px",
+            }}
+            alt=""
+            src={freePriceImage}
+          />
+        </Box>
       </Box>
     </Box>
   );
