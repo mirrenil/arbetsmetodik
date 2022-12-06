6fc8b687c82d1ad8600341392034b09e743d52a6
Erikisak <erik.isak@icloud.com>
2022-12-06 10:31:04 +0100

    #44 Validation Sign in and Sign up

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,32 +1,44 @@
-import { Typography, Box, TextField, Button, Alert } from "@mui/material";
+import { Typography, Box, TextField, Button } from "@mui/material";
 import React, { FormEvent, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../Assets/FormStyle.css";
 import { useAuth } from "../Contexts/AuthContext";
 import GoogleButton from "react-google-button";
+import * as yup from "yup";
+import { useFormik } from 'formik';
+
+const validationSchema = yup
+  .object({
+  email: yup
+  .string()
+  .required('Please enter your email address'),
+  password: yup
+    .string()
+    .required('Please enter a password'),
+  confirmPassword: yup
+    .string()
+    .required('Please confirm your password'),
+});
 
 function SignUpPage() {
   const { signup, setRegisterEmail, setRegisterPassword, googleSignIn } =
     useAuth();
-  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const emailRef = useRef<null | HTMLInputElement>(null);
   const passwordRef = useRef<null | HTMLInputElement>(null);
   const passwordConfirmationRef = useRef<null | HTMLInputElement>(null);
   const navigate = useNavigate();
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
     if (passwordRef.current?.value !== passwordConfirmationRef.current?.value) {
-      return setError("Passwords do not match");
+      return console.log("Passwords do not match");
     }
     try {
-      setError("");
       setLoading(true);
       await signup(emailRef, passwordRef);
       navigate("/signin");
     } catch (error) {
-      setError("Failed to create an account");
+      console.log("error")
     }
     setLoading(false);
   };
@@ -41,6 +53,18 @@ function SignUpPage() {
     }
   };
 
+  const formik = useFormik({
+    initialValues: {
+        email: "",
+        password: "", 
+        confirmPassword: ""
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      handleSubmit()
+    },
+});
+
   return (
     <Box sx={{
       mt:{xs: 4, md: 10, lg: 10, xl: 10}
@@ -48,45 +72,47 @@ function SignUpPage() {
       <Typography variant="h4" align="center" mb={5}>
         New to Chubby Dog?
       </Typography>
-      <Box
-        component="form"
-        className="box"
-        sx={{
-          "& > :not(style)": { m: 1, width: "30ch" },
-        }}
-        onSubmit={handleSubmit}
-        noValidate
-        autoComplete="off"
-      >
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <form onSubmit={formik.handleSubmit} style={{ display: "flex", flexDirection: "column", width: "15rem", gap: "1rem" }}>
         <TextField
-          id="outlined-basic"
+          id="email"
+          name="email"
           label="Email"
-          variant="outlined"
-          required
+          type="text"
+          value={formik.values.email}
+          onChange={ (e) =>{ 
+            formik.handleChange(e)
+            setRegisterEmail( e.target.value)
+          }}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
           ref={emailRef}
-          onChange={(e) => setRegisterEmail(e.target.value)}
         />
-
         <TextField
-          id="outlined-basic"
+          id="password"
+          name="password"
           label="Password"
-          variant="outlined"
           type="password"
-          required
+          value={formik.values.password}
+          onChange={ (e) =>{ 
+            formik.handleChange(e)
+            setRegisterPassword(e.target.value)
+          }}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
           ref={passwordRef}
-          onChange={(e) => setRegisterPassword(e.target.value)}
         />
 
         <TextField
-          id="outlined-basic"
-          label="Password confirmation"
-          variant="outlined"
+          id="confirmPassword"
+          name="confirmPassword"
+          label="Confirm Password"
           type="password"
-          ref={passwordConfirmationRef}
-          required
-          onChange={(e) => setRegisterPassword(e.target.value)}
+          value={formik.values.confirmPassword}
+          onChange={formik.handleChange}
+          error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+          helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
         />
-        {error && <Alert severity="error">{error}</Alert>}
         <Button
           type="submit"
           color="primary"
@@ -100,6 +126,7 @@ function SignUpPage() {
           OR
         </Typography>
         <GoogleButton onClick={handleGoogleSignIn} />
+      </form>
       </Box>
     </Box>
   );
