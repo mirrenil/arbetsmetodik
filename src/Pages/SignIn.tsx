8e90f7bf34ed055ac10b1bd3485fd1d728c7a056
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-12-12 11:20:54 +0100

    adds min 6 charachter to password validation

--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Typography, Button, SxProps } from "@mui/material";
@@ -11,121 +11,133 @@ import * as yup from "yup";
 import { useFormik } from "formik";
 
 const validationSchema = yup.object({
-  email: yup.string().required("Please enter your email address"),
-  password: yup.string().required("Please enter a password"),
+    email: yup.string().required("Please enter your email address"),
+    password: yup
+        .string()
+        .min(6, "The password must be atleast 6 characters")
+        .required("Please enter a password"),
 });
 
 function SignInPage() {
-  const { currentUser, login, errorMessage } = useAuth();
-  const [loginEmail, setLoginEmail] = useState<string>("");
-  const [loginPassword, setLoginPassword] = useState<string>("");
+    const { currentUser, login, errorMessage } = useAuth();
+    const [loginEmail, setLoginEmail] = useState<string>("");
+    const [loginPassword, setLoginPassword] = useState<string>("");
 
-  const handleSignIn = async () => {
-    try {
-      await login(loginEmail, loginPassword);
-    } catch (error) {
-      console.error("login failed" + error);
-    }
-  };
+    const handleSignIn = async () => {
+        try {
+            await login(loginEmail, loginPassword);
+        } catch (error) {
+            console.error("login failed" + error);
+        }
+    };
 
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    validationSchema: validationSchema,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    onSubmit: (values) => {
-      handleSignIn();
-    },
-  });
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: validationSchema,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        onSubmit: (values) => {
+            handleSignIn();
+        },
+    });
 
-  return (
-    <Box sx={wrapper}>
-      <ToastContainer />
-      <Typography variant="h4" align="center" mb={5}>
-        Welcome to Chubby Dog
-      </Typography>
-      {currentUser ? (
-        <Typography variant="h5" align="center">
-          {" "}
-          You are already signed in
-        </Typography>
-      ) : (
-        <Box>
-          <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <form
-              onSubmit={formik.handleSubmit}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                width: "15rem",
-                gap: "1rem",
-              }}
-            >
-              <TextField
-                id="email"
-                name="email"
-                label="Email"
-                type="text"
-                value={formik.values.email}
-                onChange={(e) => {
-                  formik.handleChange(e);
-                  setLoginEmail(e.target.value);
-                }}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-                helperText={formik.touched.email && formik.errors.email}
-              />
-              <TextField
-                id="password"
-                name="password"
-                label="Password"
-                type="password"
-                value={formik.values.password}
-                onChange={(e) => {
-                  formik.handleChange(e);
-                  setLoginPassword(e.target.value);
-                }}
-                error={
-                  formik.touched.password && Boolean(formik.errors.password)
-                }
-                helperText={formik.touched.password && formik.errors.password}
-              />
-              <Button
-                color="primary"
-                variant="contained"
-                sx={{ background: "#00C4BA" }}
-                type="submit"
-              >
-                Sign in
-              </Button>
-              {errorMessage ? (
-                <Typography align="center" color="red">
-                  Email and password do not match
+    return (
+        <Box sx={wrapper}>
+            <ToastContainer />
+            <Typography variant="h4" align="center" mb={5}>
+                Welcome to Chubby Dog
+            </Typography>
+            {currentUser ? (
+                <Typography variant="h5" align="center">
+                    {" "}
+                    You are already signed in
                 </Typography>
-              ) : null}
-              <Link to="/signup">
-                <Typography>
-                  Don&apos;t have an account? Sign up here!
-                </Typography>
-              </Link>
-            </form>
-          </Box>
+            ) : (
+                <Box>
+                    <Box sx={{ display: "flex", justifyContent: "center" }}>
+                        <form
+                            onSubmit={formik.handleSubmit}
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                width: "15rem",
+                                gap: "1rem",
+                            }}
+                        >
+                            <TextField
+                                id="email"
+                                name="email"
+                                label="Email"
+                                type="text"
+                                value={formik.values.email}
+                                onChange={(e) => {
+                                    formik.handleChange(e);
+                                    setLoginEmail(e.target.value);
+                                }}
+                                error={
+                                    formik.touched.email &&
+                                    Boolean(formik.errors.email)
+                                }
+                                helperText={
+                                    formik.touched.email && formik.errors.email
+                                }
+                            />
+                            <TextField
+                                id="password"
+                                name="password"
+                                label="Password"
+                                type="password"
+                                value={formik.values.password}
+                                onChange={(e) => {
+                                    formik.handleChange(e);
+                                    setLoginPassword(e.target.value);
+                                }}
+                                error={
+                                    formik.touched.password &&
+                                    Boolean(formik.errors.password)
+                                }
+                                helperText={
+                                    formik.touched.password &&
+                                    formik.errors.password
+                                }
+                            />
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                sx={{ background: "#00C4BA" }}
+                                type="submit"
+                            >
+                                Sign in
+                            </Button>
+                            {errorMessage ? (
+                                <Typography align="center" color="red">
+                                    Email and password do not match
+                                </Typography>
+                            ) : null}
+                            <Link to="/signup">
+                                <Typography>
+                                    Don&apos;t have an account? Sign up here!
+                                </Typography>
+                            </Link>
+                        </form>
+                    </Box>
+                </Box>
+            )}
         </Box>
-      )}
-    </Box>
-  );
+    );
 }
 
 const wrapper: SxProps = {
-  flexDirection: "column",
-  position: { xs: "static", md: "relative", lg: "relative", xl: "relative" },
-  top: { xs: "0", md: "150px", lg: "100px", xl: "100px" },
-  width: "100%",
-  minHeight: "700px",
-  display: "flex",
-  justifyContent: "center",
-  zIndex: "100",
+    flexDirection: "column",
+    position: { xs: "static", md: "relative", lg: "relative", xl: "relative" },
+    top: { xs: "0", md: "150px", lg: "100px", xl: "100px" },
+    width: "100%",
+    minHeight: "700px",
+    display: "flex",
+    justifyContent: "center",
+    zIndex: "100",
 };
 
 export default SignInPage;
--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,5 +1,5 @@
 import { Typography, Box, TextField, Button } from "@mui/material";
-import React, { FormEvent, useState } from "react";
+import React, { useState } from "react";
 import "../Assets/FormStyle.css";
 import { useAuth } from "../Contexts/AuthContext";
 import { ToastContainer } from "react-toastify";
@@ -8,146 +8,165 @@ import * as yup from "yup";
 import { useFormik } from "formik";
 
 const validationSchema = yup.object({
-  email: yup.string().required("Please enter your email address"),
-  displayName: yup.string().required("Display name is required"),
-  password: yup.string().required("Please enter a password"),
-  confirmPassword: yup.string().required("Please confirm your password"),
+    email: yup.string().required("Please enter your email address"),
+    displayName: yup.string().required("Display name is required"),
+    password: yup
+        .string()
+        .min(6, "The password must be atleast 6 characters")
+        .required("Please enter a password"),
+    confirmPassword: yup
+        .string()
+        .min(6, "The password must be atleast 6 characters")
+        .required("Please confirm your password"),
 });
 
 function SignUpPage() {
-  const { signup, errorMessage } = useAuth();
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [confirmationPassword, setConfirmationPassword] = useState<string>("");
-  const [displayName, setDisplayedName] = useState<string>("");
+    const { signup, errorMessage } = useAuth();
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [confirmationPassword, setConfirmationPassword] =
+        useState<string>("");
+    const [displayName, setDisplayedName] = useState<string>("");
 
-  const handleSignUp = async () => {
-    if (password !== confirmationPassword) {
-      return alert("Passwords do not match");
-    }
+    const handleSignUp = async () => {
+        if (password !== confirmationPassword) {
+            return alert("Passwords do not match");
+        }
 
-    try {
-      await signup(email, password, displayName);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+        try {
+            await signup(email, password, displayName);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      displayName,
-      password: "",
-      confirmPassword: "",
-    },
-    validationSchema: validationSchema,
-    // eslint-disable-next-line
-    onSubmit: (values) => {
-      handleSignUp();
-    },
-  });
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            displayName,
+            password: "",
+            confirmPassword: "",
+        },
+        validationSchema: validationSchema,
+        // eslint-disable-next-line
+        onSubmit: (values) => {
+            handleSignUp();
+        },
+    });
 
-  return (
-    <Box
-      sx={{
-        mt: { xs: 4, md: 10, lg: 10, xl: 10 },
-      }}
-    >
-      <ToastContainer />
-      <Typography variant="h4" align="center" mb={5}>
-        New to Chubby Dog?
-      </Typography>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            formik.handleSubmit();
-          }}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            width: "15rem",
-            gap: "1rem",
-          }}
+    return (
+        <Box
+            sx={{
+                mt: { xs: 4, md: 10, lg: 10, xl: 10 },
+            }}
         >
-          <TextField
-            id="email"
-            name="email"
-            label="Email"
-            type="text"
-            value={formik.values.email}
-            onChange={(e) => {
-              formik.handleChange(e);
-              setEmail(e.target.value);
-            }}
-            error={formik.touched.email && Boolean(formik.errors.email)}
-            helperText={formik.touched.email && formik.errors.email}
-          />
-          <TextField
-            id="displayName"
-            label="Display name"
-            name="displayName"
-            type="text"
-            onChange={(e) => {
-              formik.handleChange(e);
-              setDisplayedName(e.target.value);
-            }}
-            value={formik.values.displayName}
-            error={
-              formik.touched.displayName && Boolean(formik.errors.displayName)
-            }
-            helperText={formik.touched.displayName && formik.errors.displayName}
-          />
-          <TextField
-            id="password"
-            name="password"
-            label="Password"
-            type="password"
-            value={formik.values.password}
-            onChange={(e) => {
-              formik.handleChange(e);
-              setPassword(e.target.value);
-            }}
-            error={formik.touched.password && Boolean(formik.errors.password)}
-            helperText={formik.touched.password && formik.errors.password}
-          />
-
-          <TextField
-            id="confirmPassword"
-            name="confirmPassword"
-            label="Confirm Password"
-            type="password"
-            value={formik.values.confirmPassword}
-            onChange={(e) => {
-              formik.handleChange(e);
-              setConfirmationPassword(e.target.value);
-            }}
-            error={
-              formik.touched.confirmPassword &&
-              Boolean(formik.errors.confirmPassword)
-            }
-            helperText={
-              formik.touched.confirmPassword && formik.errors.confirmPassword
-            }
-          />
-          {errorMessage ? (
-            <Typography color="red" align="center">
-              Something went wrong, please try again
+            <ToastContainer />
+            <Typography variant="h4" align="center" mb={5}>
+                New to Chubby Dog?
             </Typography>
-          ) : null}
-          <Button
-            type="submit"
-            color="primary"
-            variant="contained"
-            sx={{ background: "#00C4BA", color: "white" }}
-            className="buttonStyle"
-          >
-            Sign up
-          </Button>
-        </form>
-      </Box>
-    </Box>
-  );
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        formik.handleSubmit();
+                    }}
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        width: "15rem",
+                        gap: "1rem",
+                    }}
+                >
+                    <TextField
+                        id="email"
+                        name="email"
+                        label="Email"
+                        type="text"
+                        value={formik.values.email}
+                        onChange={(e) => {
+                            formik.handleChange(e);
+                            setEmail(e.target.value);
+                        }}
+                        error={
+                            formik.touched.email && Boolean(formik.errors.email)
+                        }
+                        helperText={formik.touched.email && formik.errors.email}
+                    />
+                    <TextField
+                        id="displayName"
+                        label="Display name"
+                        name="displayName"
+                        type="text"
+                        onChange={(e) => {
+                            formik.handleChange(e);
+                            setDisplayedName(e.target.value);
+                        }}
+                        value={formik.values.displayName}
+                        error={
+                            formik.touched.displayName &&
+                            Boolean(formik.errors.displayName)
+                        }
+                        helperText={
+                            formik.touched.displayName &&
+                            formik.errors.displayName
+                        }
+                    />
+                    <TextField
+                        id="password"
+                        name="password"
+                        label="Password"
+                        type="password"
+                        value={formik.values.password}
+                        onChange={(e) => {
+                            formik.handleChange(e);
+                            setPassword(e.target.value);
+                        }}
+                        error={
+                            formik.touched.password &&
+                            Boolean(formik.errors.password)
+                        }
+                        helperText={
+                            formik.touched.password && formik.errors.password
+                        }
+                    />
+
+                    <TextField
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        label="Confirm Password"
+                        type="password"
+                        value={formik.values.confirmPassword}
+                        onChange={(e) => {
+                            formik.handleChange(e);
+                            setConfirmationPassword(e.target.value);
+                        }}
+                        error={
+                            formik.touched.confirmPassword &&
+                            Boolean(formik.errors.confirmPassword)
+                        }
+                        helperText={
+                            formik.touched.confirmPassword &&
+                            formik.errors.confirmPassword
+                        }
+                    />
+                    {errorMessage ? (
+                        <Typography color="red" align="center">
+                            Something went wrong, please try again
+                        </Typography>
+                    ) : null}
+                    <Button
+                        type="submit"
+                        color="primary"
+                        variant="contained"
+                        sx={{ background: "#00C4BA", color: "white" }}
+                        className="buttonStyle"
+                    >
+                        Sign up
+                    </Button>
+                </form>
+            </Box>
+        </Box>
+    );
 }
 
 export default SignUpPage;
