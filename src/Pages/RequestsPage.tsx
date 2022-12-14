846b4577f9ab48f93c463f558a237093bd566d67
Hazem Kawas <hazem.k.4488@gmail.com>
2022-12-14 10:05:12 +0100

    add conditional rendering for the requests page

--- src/Pages/RequestsPage.tsx
+++ src/Pages/RequestsPage.tsx
@@ -1,86 +1,119 @@
 /* eslint-disable */
-import { Box, SxProps, Typography } from "@mui/material";
+import { Box, Button, SxProps, Typography } from "@mui/material";
 import React from "react";
 import RequestCard from "../Components/RequestCard";
 import { useUser } from "../Contexts/UserContext";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { useAuth } from "../Contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function RequestsPage() {
-  const { myReceivedRequests, mySentRequests } = useUser();
+    const { myReceivedRequests, mySentRequests } = useUser();
+    const { currentUser } = useAuth();
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <Box sx={wrapper}>
-        <Typography sx={titleStyle}>Recieved requests:</Typography>
-        {myReceivedRequests.length ? (
-          <Box sx={cardsContainer}>
-            {myReceivedRequests.map((req) => {
-              return (
-                <RequestCard
-                  key={req.id}
-                  request={req}
-                  isMySentRequest={false}
-                />
-              );
-            })}
-            {myReceivedRequests.length > 1 ? (<ArrowForwardIosIcon
-              style={{
-                position: "sticky",
-                right: '10px',
-                color: "black",
-              }}
-            />) : (
-              null
+    return (
+        <>
+            {currentUser ? (
+                <Box sx={wrapper}>
+                    <Typography sx={titleStyle}>Recieved requests:</Typography>
+                    {myReceivedRequests.length ? (
+                        <Box sx={cardsContainer}>
+                            {myReceivedRequests.map((req) => {
+                                return (
+                                    <RequestCard
+                                        key={req.id}
+                                        request={req}
+                                        isMySentRequest={false}
+                                    />
+                                );
+                            })}
+                            {myReceivedRequests.length > 1 ? (
+                                <ArrowForwardIosIcon
+                                    style={{
+                                        position: "sticky",
+                                        right: "10px",
+                                        color: "black",
+                                    }}
+                                />
+                            ) : null}
+                        </Box>
+                    ) : null}
+                    <Typography sx={titleStyle}>Sent requests:</Typography>
+                    <Box sx={cardsContainer}>
+                        {mySentRequests.map((req) => {
+                            return (
+                                <RequestCard
+                                    key={req.id}
+                                    request={req}
+                                    isMySentRequest={true}
+                                />
+                            );
+                        })}
+                        {mySentRequests.length > 1 ? (
+                            <ArrowForwardIosIcon
+                                style={{
+                                    position: "sticky",
+                                    right: "10px",
+                                    color: "black",
+                                }}
+                            />
+                        ) : null}
+                    </Box>
+                </Box>
+            ) : (
+                <Box sx={notSignedIn}>
+                    <Typography align="center" variant="h6">
+                        You are not signed in, please sign in to be able to send
+                        a booking request.
+                    </Typography>
+                    <Button
+                        sx={button}
+                        variant="contained"
+                        onClick={() => navigate("/signin")}
+                    >
+                        Sign in
+                    </Button>
+                </Box>
             )}
-
-          </Box>
-        ) : (
-          null
-        )}
-        <Typography sx={titleStyle}>Sent requests:</Typography>
-        <Box sx={cardsContainer}>
-          {mySentRequests.map((req) => {
-            return (
-              <RequestCard
-                key={req.id}
-                request={req}
-                isMySentRequest={true}
-              />
-            );
-          })}
-          {mySentRequests.length > 1 ? (<ArrowForwardIosIcon
-            style={{
-              position: "sticky",
-              right: '10px',
-              color: "black",
-            }}
-          />) : (
-            null
-          )}
-        </Box>
-      </Box>
-    </>
-  );
+        </>
+    );
 }
 
 const wrapper: SxProps = {
-  margin: "7rem 0",
-  minHeight: "100%",
+    margin: "7rem 0",
+    minHeight: "100%",
 };
 const titleStyle: SxProps = {
-  textAlign: "center",
-  fontSize: { xs: "1.2rem", md: "2rem" },
-  marginTop: { xs: "none", md: "12rem", lg: "2rem", xl: "2rem" },
-  marginBottom: "1rem",
+    textAlign: "center",
+    fontSize: { xs: "1.2rem", md: "2rem" },
+    marginTop: { xs: "none", md: "12rem", lg: "2rem", xl: "2rem" },
+    marginBottom: "1rem",
 };
 
 const cardsContainer: SxProps = {
-  display: "flex",
-  alignItems: "center",
-  width: "100%",
-  height: "20rem",
-  overflowX: "scroll",
-  position: "relative",
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    height: "20rem",
+    overflowX: "scroll",
+    position: "relative",
+};
+const notSignedIn: SxProps = {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    height: "20rem",
+    position: "relative",
+    flexDirection: "column",
+    mt: 30,
+};
+const button: SxProps = {
+    marginTop: "1rem",
+    marginBottom: "2rem",
+    width: "30%",
+    height: "15%",
+    color: "white",
 };
 
 export default RequestsPage;
